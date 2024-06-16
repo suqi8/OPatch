fa0b367b10c4d53725913d198e1496e1b8241480
suqi8 <3383787570@qq.com>
2024-06-16 16:23:33 +0800

    (Update AndroidManifest.xml to remove obsolete attributes and adjust activities orientation)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = property.project.app.versionName
         versionCode = property.project.app.versionCode
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
     buildTypes {
         release {
@@ -38,18 +41,38 @@ android {
     buildFeatures {
         buildConfig = true
         viewBinding = true
+        compose = true
     }
     lint { checkReleaseBuilds = false }
     ndkVersion = "27.0.11718014 rc1"
     buildToolsVersion = "34.0.0"
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
     // TODO Please visit https://highcapable.github.io/YukiHookAPI/en/api/special-features/host-inject
     // TODO 请参考 https://highcapable.github.io/YukiHookAPI/zh-cn/api/special-features/host-inject
     // androidResources.additionalParameters += listOf("--allow-reserved-package-id", "--package-id", "0x64")
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     compileOnly(de.robv.android.xposed.api)
     implementation(com.highcapable.yukihookapi.api)
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
     ksp(com.highcapable.yukihookapi.ksp.xposed)
     implementation(com.github.duanhong169.drawabletoolbox)
     implementation(androidx.core.core.ktx)
