f0fd84e865409255e8125bf388f953ecc686363b
suqi8 <3383787570@qq.com>
2024-06-16 11:37:45 +0800

    首次提交

---
+++ app/src/main/java/io/github/suqi8/opatch/hook/HookEntry.kt
@@ -0,0 +1,18 @@
+package io.github.suqi8.opatch.hook
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed(entryClassName = "opatch", isUsingResourcesHook = true)
+class HookEntry : IYukiHookXposedInit {
+
+    override fun onInit() = configs {
+        // Your code here.
+    }
+
+    override fun onHook() = encase {
+        // Your code here.
+    }
+}
