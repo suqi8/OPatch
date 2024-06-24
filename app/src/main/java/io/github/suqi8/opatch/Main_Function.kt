6c884c9b3719572fc4dc1d7903b997f471ffba27
suqi8 <3383787570@qq.com>
2024-06-24 22:47:09 +0800

    更新 Main_Function.kt 以实现应用图标点击跳转功能

--- app/src/main/java/io/github/suqi8/opatch/Main_Function.kt
+++ app/src/main/java/io/github/suqi8/opatch/Main_Function.kt
@@ -1,28 +1,39 @@
 package io.github.suqi8.opatch
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 
+const val PACKAGE_android = "android"
 
 @Composable
 fun Main_Function() {
+    val context = LocalContext.current
     Column {
-        GetAppIconAndName(packageName = "android") { appName, icon ->
-            Row(modifier = Modifier.padding(16.dp),verticalAlignment =  Alignment.CenterVertically) {
-                Image(bitmap = icon, contentDescription = "App Icon")
+        GetAppIconAndName(packageName = PACKAGE_android) { appName, icon ->
+            Row(modifier = Modifier
+                .clickable { //跳转到Fun_android页面
+                    startActivity(context, Intent(context, Fun_android::class.java), null)
+                     }
+                .fillMaxWidth(),verticalAlignment =  Alignment.CenterVertically) {
+                Image(bitmap = icon, contentDescription = "App Icon", modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 16.dp))
                 Column(verticalArrangement =  Arrangement.Center, modifier = Modifier.padding(start = 16.dp)) {
                     Text(text = appName)
-                    Text(text = "Package Name: android")
+                    Text(text = PACKAGE_android)
                 }
             }
         }
