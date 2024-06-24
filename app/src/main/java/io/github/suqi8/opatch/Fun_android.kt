18cfee9635c9cf1cd2fe414d3f3ba443f69cf12e
suqi8 <3383787570@qq.com>
2024-06-24 22:47:15 +0800

    更新 Fun_android.kt 以实现新的 Android 应用界面

---
+++ app/src/main/java/io/github/suqi8/opatch/Fun_android.kt
@@ -0,0 +1,47 @@
+package io.github.suqi8.opatch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.suqi8.opatch.ui.theme.OPatchTheme
+
+class Fun_android : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OPatchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OPatchTheme {
+        Greeting2("Android")
+    }
+}
