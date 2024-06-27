b57dec935090bc6f1262fe05e17bf1e462c650e0
suqi8 <3383787570@qq.com>
2024-06-27 15:19:02 +0800

    更新 app/src/main/java/io/github/suqi8/opatch/hook/ReturnConstant.java 和 XposedHelper.java 以实现新的 Xposed 模块功能

---
+++ app/src/main/java/io/github/suqi8/opatch/hook/ReturnConstant.java
@@ -0,0 +1,24 @@
+package io.github.suqi8.opatch.hook;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+
+public class ReturnConstant extends XC_MethodHook {
+    private final XSharedPreferences prefs;
+    private final String prefsKey;
+    private final Object value;
+
+    public ReturnConstant(XSharedPreferences prefs, String prefsKey, Object value) {
+        this.prefs = prefs;
+        this.prefsKey = prefsKey;
+        this.value = value;
+    }
+
+    @Override
+    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+        prefs.reload();
+        if (prefs.getBoolean(prefsKey, true)) {
+            param.setResult(value);
+        }
+    }
+}
