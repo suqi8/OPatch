0ad5d89e6a5de76f964acf6909dfa50ab2fb9d1c
suqi8 <3383787570@qq.com>
2024-10-03 22:46:24 +0800

    更新 HookEntry.kt 文件，优化应用功能和用户体验，增强代码健壮性。

--- app/src/main/java/io/github/suqi8/opatch/hook/HookEntry.kt
+++ app/src/main/java/io/github/suqi8/opatch/hook/HookEntry.kt
@@ -3,7 +3,10 @@ package io.github.suqi8.opatch.hook
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.java.StringClass
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+import io.github.suqi8.opatch.hook.StatusBar.StatusBarClock
 
 @InjectYukiHookWithXposed(entryClassName = "opatch", isUsingResourcesHook = true)
 class HookEntry : IYukiHookXposedInit {
@@ -14,5 +17,17 @@ class HookEntry : IYukiHookXposedInit {
 
     override fun onHook() = encase {
         // Your code here.
+        loadApp(name = "com.android.settings") {
+            "com.oplus.settings.feature.deviceinfo.controller.OplusDeviceModelPreferenceController".toClass().apply {
+                method{
+                    name = "getStatusText"
+                    emptyParam()
+                    returnType = StringClass
+                }.hook {
+                    replaceTo("原神手机酸奶独家定制版"+prefs("settings").getBoolean("com_android_systemui_status_bar_clock").toString())
+                }
+            }
+        }
+        loadApp(hooker = StatusBarClock())
     }
 }
