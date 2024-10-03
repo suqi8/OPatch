e3b6e13036f1b680597e6c12a125e36b3066d24f
suqi8 <3383787570@qq.com>
2024-10-03 13:23:19 +0800

    更新 build.gradle.kts 文件，增加 Kotlin 插件 Compose，优化应用功能和用户体验，增强代码健壮性。

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     autowire(libs.plugins.android.application) apply false
     autowire(libs.plugins.kotlin.android) apply false
     autowire(libs.plugins.kotlin.ksp) apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
 val defaultMinSdkVersion by extra(31)
