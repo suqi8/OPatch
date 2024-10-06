d94d28b8b84afc40936886c6bde73de7e94df640
suqi8 <3383787570@qq.com>
2024-10-06 11:03:31 +0800

    修复上传APK脚本，按架构分别上传，并增加对output-metadata.json的支持。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
         abi {
             isEnable = true
             reset()
-            include("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+            include("arm64-v8a", "armeabi-v7a")
             isUniversalApk = true
         }
     }
