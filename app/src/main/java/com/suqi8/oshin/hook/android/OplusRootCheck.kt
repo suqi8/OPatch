217b4671bb438609a49fcd5b652a4dc7b657caa1
suqi8 <3383787570@qq.com>
2025-01-24 21:23:00 +0800

    refactor(ui): 重构 Oplus 服务设置界面
    - 将 Oplus 服务设置界面迁移到 FunPage 组件中
    - 使用 FunSwich 组件替换原有的 SuperSwitch 组件
    - 移除冗余代码，提高代码可读性

--- app/src/main/java/com/suqi8/oshin/hook/android/OplusRootCheck.kt
+++ app/src/main/java/com/suqi8/oshin/hook/android/OplusRootCheck.kt
@@ -6,7 +6,7 @@ import com.highcapable.yukihookapi.hook.factory.method
 class OplusRootCheck : YukiBaseHooker() {
     override fun onHook() {
         loadSystem {
-            if (prefs("oplus_system_services").getBoolean("disable_root_check", false)) {
+            if (prefs("android\\oplus_system_services").getBoolean("disable_root_check", false)) {
                 "com.android.server.oplus.heimdall.HeimdallService".toClass().apply {
                     method {
                         name = "isRootEnable"
